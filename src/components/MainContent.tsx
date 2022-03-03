0c13631bf0006643e8552a4bca1380b0721cbe07
laura J <laura@academy.com>
2022-03-03 00:04:38 +0000

    added 3 other places

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -16,36 +16,36 @@ function MainContent(): JSX.Element {
         }
       />
       <Place
-        title={"McLeod Lake"}
-        placeName={"Mcleod Lake"}
-        countryName={"British Columbia"}
-        mainImage={"https://unsplash.com/photos/oMneOBYhJxY"}
-        imageAlt={"A lake in the mountains"}
-        locationMap={"https://goo.gl/maps/rg3UF4WUfEhjGAe98"}
+        title={"Matthias Church"}
+        placeName={"Budapest"}
+        countryName={"Hungary"}
+        mainImage={"https://unsplash.com/photos/2TmsyZXMNTE"}
+        imageAlt={"Budapest Central Square"}
+        locationMap={"https://goo.gl/maps/WdE1mHHxPRdv9Dw38"}
         description={
-          "Ice melt blue lakes, endless forrests and mountains that touch the sky. "
+          "A bustling cultural square filled with beautiful architecture."
         }
       />
       <Place
-        title={"McLeod Lake"}
-        placeName={"Mcleod Lake"}
-        countryName={"British Columbia"}
-        mainImage={"https://unsplash.com/photos/oMneOBYhJxY"}
-        imageAlt={"A lake in the mountains"}
-        locationMap={"https://goo.gl/maps/rg3UF4WUfEhjGAe98"}
+        title={"Betws-y-Coed"}
+        placeName={"Wales"}
+        countryName={"UK"}
+        mainImage={"https://unsplash.com/photos/Ddp_T92Vjus"}
+        imageAlt={"A footpath in the mountains."}
+        locationMap={"https://goo.gl/maps/rw2KgmJ258w2hNQz6"}
         description={
-          "Ice melt blue lakes, endless forrests and mountains that touch the sky. "
+          "Long walks, spectacular scenery and 10/10 cream teas!. "
         }
       />
       <Place
-        title={"McLeod Lake"}
-        placeName={"Mcleod Lake"}
-        countryName={"British Columbia"}
-        mainImage={"https://unsplash.com/photos/oMneOBYhJxY"}
-        imageAlt={"A lake in the mountains"}
-        locationMap={"https://goo.gl/maps/rg3UF4WUfEhjGAe98"}
+        title={"Sheffield"}
+        placeName={"England"}
+        countryName={"UK"}
+        mainImage={"https://unsplash.com/photos/P4ALLoVYiZI"}
+        imageAlt={"The industrial city of Sheffield."}
+        locationMap={"https://goo.gl/maps/k9HVz1zmgEHGV35L9"}
         description={
-          "Ice melt blue lakes, endless forrests and mountains that touch the sky. "
+          "Hidden in the Peak District, Sheffield is the hidden gem of the North. "
         }
       />
     </main>
